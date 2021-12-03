af2bb13186ed0d432d6035af50f606e2d6167527
iqubb <vydrenkoyegor@gmail.com>
2021-12-03 16:38:48 +0300

    add book server

--- src/user/main.go
+++ src/user/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	pb "github.com/iqubb/src/user/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = "5001"
+)
+
 func main() {
 
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &UserServer{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
